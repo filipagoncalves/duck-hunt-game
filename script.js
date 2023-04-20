bd69047b00785e427438a4d0b811f30d6b939c86
filipagoncalves <filipa.i.goncalves@gmail.com>
2023-04-20 17:58:37 +0100

    - animations of duck resolved - Score interface implemented :)

--- script.js
+++ script.js
@@ -3,25 +3,127 @@ const windowH = window.innerHeight;
 const windowW = window.innerWidth;
 const maxX = windowW - duck.offsetWidth;
 const maxY = windowH - duck.offsetHeight;
-let duckMooves = [];
+let mouseX, mouseY = 0;
+let bullet, miniDuck = null;
+const scoreValues = [500, 1000, 1500];
+const duckIsDead = false;
 
-const startGame = () => {
+const startGame = async () => {
 
-    animateDog();
-    createDuck();
+    await animateDog();
+    await createDuck();
+    gameLogic();
 
-    
-    //move(duckPath(maxY));
+    verifyShot();
 }
 
 const gameLogic = () => {
-    
+
+    /*while(true){
+        //verificar se um pato foi morto
+        verifyShot();
+       
+        //verificar se ainda tem tiros disponíveis
+        verifyBullets();
+
+        
+    }
+
+    createDuck();
+    gameOver();*/
 }
 
+
+
+let nrBullets = 3;
+let shotDuck = false;
+let duckShoted = 1;
+
+const verifyShot = () => {
+
+    console.log("let's kill some ducks");    
+
+    // listen for clicks on the document
+    document.addEventListener('click', (event) => {
+        
+        const elementRect = duck.getBoundingClientRect();
+        removeBullet();
+   
+        // check if the mouse coordinates are within the element's rectangle
+        if (event.clientX >= elementRect.left && event.clientX <= elementRect.right
+            && event.clientY >= elementRect.top && event.clientY <= elementRect.bottom) {
+            // the mouse was clicked within the element's rectangle
+            console.log('You killed a duck!');
+            
+            stopAnimation();
+            shotDuck = true;
+        } else {
+            nrBullets--;
+            shotDuck = false;
+            console.log('You missed!');
+        }
+    });
+    
+}
+//verifyShoot();
+
+const verifyBullets = () => nrBullets === 0 ? false : true;
+
+const removeBullet = () => {
+    bullet = document.querySelector("#shots :nth-child(" + nrBullets + ")");
+    bullet.style.visibility = "hidden";
+}
+
+const showBullet = () => {
+    bullet = document.querySelector("#shots :nth-child(" + nrBullets + ")");
+    bullet.style.visibility = "visible";
+}
+
+const blinkDuck = () => {
+    miniDuck = document.querySelector("#ducks :nth-child(" + duckShoted + ")");
+    miniDuck.classList.add('blink');
+}
+
+const removeBlinkDuck = () => {
+    miniDuck = document.querySelector("#ducks :nth-child(" + duckShoted + ")");
+    miniDuck.classList.remove('blink');
+}
+
+const miniDuckPosition = () => {
+    miniDuck = document.querySelector("#ducks :nth-child(" + duckShoted + ")");
+    miniDuck.style.backgroundPosition = "-27px 582px";
+}
+
+
+/*window.addEventListener('click', (event) => {
+    mousePos = { x: event.clientX, y: event.clientY };
+    console.log(mousePos);
+});*/
+
+
+
 const createDuck = () => {
+
+    
     duckInitialPosition();
     animateScript("duck", 0, -230, 80, 160, 200);
     duckAnimation(duckPath(maxX), duckPath(maxY));
+
+    return new Promise((resolve) => {
+        console.log("second");
+        resolve();
+      });
+}
+
+
+const animateDog = () => {
+
+    return new Promise((resolve) => {
+        console.log("1st");
+        resolve();
+      });
+
+    //console.log("Dog has been created!");
 }
 
 let dogAnim;
@@ -168,11 +270,46 @@ const duckAnimation = (valueX, valueY) => {
 const stopAnimation = () => {
     animation.pause();
     clearInterval(tID);
+    fallingDuckAnimation();
+}
 
-    duck.style.backgroundPosition = '0px -460px'; 
+const fallingDuckAnimation = () => {
 
-    //animateScript("duck", 0, -460, 65, 65, 500);
-    duckAnimation(duck.offsetLeft, window.innerHeight);
+    let endDuck = setInterval ( () => {
+
+        duck.style.backgroundPosition = '-100px -460px';
+        duck.style.width = '35px';
+        shotedDuckAnimation();
+
+        duck.animate([
+            {top: window.innerHeight + 'px'}],
+            {
+                duration: 1000
+            }).onfinish = () => {
+                duckIsDead = true;
+                duck.remove();
+                //cão aparece com o pato
+
+        };
+        clearInterval(endDuck);
+    }
+    , 1000 );
+    
+    duck.style.backgroundPosition = '0px -460px';
+}
+
+
+let shotAnimation = null;
+const shotedDuckAnimation = () => {
+
+    let anim = setInterval ( () => {
+        if(duckIsDead){
+            clearInterval(anim);
+        } else {
+            duck.style.transform === 'scaleX(1)' ? duck.style.transform = 'scaleX(-1)' : duck.style.transform = 'scaleX(1)';
+        }
+    }
+    , 200 );
 }
 
 const checkCoordinates = (initialX, finalX) => {
